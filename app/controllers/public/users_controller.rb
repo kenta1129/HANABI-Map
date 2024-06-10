63dcee277019ee2f97d4fb0a149bf31420271b33
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-06-10 08:27:19 +0000

    postのnew index

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,38 @@
+class Public::UsersController < ApplicationController
+
+def show
+  @user = User.find(params[:id])
+  @posts = @user.posts
+  @post = Post.new
+end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+     flash[:notice] = "You have updated user successfully."
+   redirect_to user_path(@user)
+  else
+    render :edit
+  end
+end
+
+def index
+  @user = current_user
+  @users = User.all
+  @post =Post.new
+  @posts = Post.all
+end
+
+ private
+
+  def user_params
+    params.require(:user).permit(:name )
+  end
+
+
+
+end
--- db/migrate/20240607090304_devise_create_users.rb
+++ db/migrate/20240607090304_devise_create_users.rb
@@ -6,7 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :name
+      t.boolean :is_active, null: false, default: true
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
