bd413200ffec107c64504a79ed6b7d2adb713e9c
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-06-25 13:38:06 +0000

    2ndデプロイ　テスト　完了

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,63 +1,63 @@
 class Public::PostsController < ApplicationController
-before_action :authenticate_user!
-before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   
-def index
-  @posts = Post.all
-end
+  def index
+    @posts = Post.all
+  end
 
-def new
-  @post = Post.new
-end
+  def new
+    @post = Post.new
+  end
 
-def create
+  def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-    redirect_to post_path(@post), notice: "You have created post successfully."
+      redirect_to post_path(@post), notice: "You have created post successfully."
     else
-     @posts = Post.all
-    render:index
+      @posts = Post.all
+      render :index
     end
-end
+  end
 
-def show
-   @post = Post.find(params[:id])
-   @post_comment = PostComment.new
-end
+  def show
+    @post = Post.find(params[:id])
+    @post_comment = PostComment.new
+  end
 
-def edit
-  @post = Post.find(params[:id])
-end
+  def edit
+    @post = Post.find(params[:id])
+  end
 
-def update
+  def update
     @post = Post.find(params[:id])
     @post.user_id = current_user.id
     if @post.update(post_params)
-     flash[:notice] = "You have updated post successfully."
+      flash[:notice] = "You have updated post successfully."
       redirect_to post_path(@post.id)
     else
       render :edit
     end
-end
+  end
 
-def destroy
-   @post.destroy
-   flash[:notice] = "Post was successfully destroyed."
-   redirect_to posts_path
-end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Post was successfully destroyed."
+    redirect_to posts_path
+  end
   
-private
-  
+  private
+
   def post_params
     params.require(:post).permit(:body)
   end
 
   def ensure_correct_user
-  @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     unless @post.user.id == current_user.id
       redirect_to posts_path
     end
   end
-  
 end
