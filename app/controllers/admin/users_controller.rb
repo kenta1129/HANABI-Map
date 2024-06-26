419f482bc148ae7699b7ebdf957b715062b2b0bb
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-06-26 09:14:13 +0000

    アドミンログイン関係なし

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
 
-  before_action :authenticate_admin!
+  # before_action :authenticate_admin!
   before_action :set_user, only: [:show, :edit, :update]
 
   def index
