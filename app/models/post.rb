d06e955694459270c84c794928648875404508b0
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-07-06 12:14:13 +0000

    新規投稿完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ApplicationRecord
   validates :title, {presence: true}
   validates :body, {presence: true, length: {maximum:200}}
   
-  validates :address, presence: true
+  # validates :address, presence: true
 
   geocoded_by :address
   after_validation :geocode
