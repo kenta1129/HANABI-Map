78cfbee2ddb5bde37641b3a19f8364ca9fcce9ed
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-07-13 19:00:26 +0000

    DM修正

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -4,7 +4,7 @@ class Public::ChatsController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
+    @photo_url = "dmm.png"
     rooms = current_user.user_rooms.pluck(:room_id)
 
     user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
